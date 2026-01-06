0499435c4dada35036b1736bea6d0aad8eba7063
Eman <eman.alzouabi@l-one.de>
2026-01-06 12:10:23 +0300

    Remove unused import

--- app/Repositories/Interfaces/BaseRepositoryInterface.php
+++ app/Repositories/Interfaces/BaseRepositoryInterface.php
@@ -2,7 +2,6 @@
 
 namespace App\Repositories\Interfaces;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 interface BaseRepositoryInterface{
